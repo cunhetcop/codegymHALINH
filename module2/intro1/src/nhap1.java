dc6c474d8da600a0cd492b099f9e8820c34354cf
cunhetcop <nguyenhalinh.1709@gmail.com>
2022-09-07 16:20:53 +0700

    update 2

--- module2/intro1/src/nhap1.java
+++ module2/intro1/src/nhap1.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 
 public class nhap1 {
-    public static void main(String[] args) {
-        int size;
-        int[] array;
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.print("Enter a size:");
-            size = scanner.nextInt();
-            if (size > 20)
-                System.out.println("Size does not exceed 20");
-        } while (size > 20);
+    public static boolean checkSNT(int x) {
+        return false;
     }
+    public static void main(String[] args) {
+
+        }
+
+
 }
---
+++ module2/intro1/src/them_mot_phan_tu_vao_mang.java
@@ -0,0 +1,2 @@
+public class them_mot_phan_tu_vao_mang {
+}
