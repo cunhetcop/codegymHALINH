f41c88036099f242680391e8986c112f41c68ab0
cunhetcop <nguyenhalinh.1709@gmail.com>
2022-09-14 18:25:46 +0700

    update 2

---
+++ module2/intro1/Abstract Interface/src/InterfaceHinhHoc/Circle.java
@@ -0,0 +1,14 @@
+package InterfaceHinhHoc;
+
+public class Circle {
+    public Circle(double radius) {
+
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+    }
+
+    public Circle() {
+
+    }
+}
